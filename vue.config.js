e1c8fd7154a33ac805143cb4c3ae0709d3e5b55e
erzongLiuan <liuan_qd@126.com>
2021-08-22 11:52:05 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    configureWebpack: {
+        reslove: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views',
+            }
+        }
+    }
+}
